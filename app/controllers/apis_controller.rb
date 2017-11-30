08922a345269c2cd169cce48f888653c7197fed8
shirlaine <shirlaine.phang@gmail.com>
2017-11-30 22:14:57 +0800

    Add fb-api test controller

--- app/controllers/apis_controller.rb
+++ app/controllers/apis_controller.rb
@@ -14,9 +14,12 @@ class ApisController < ApplicationController
 
   def retrieve
     # we are passing in the url_string to the GetApi service
-    @service = GetApiService.call("https://api.github.com/users/shirlaine")
+    @service = GetApiService.call("https://graph.facebook.com/v2.11/801300133319963_1476720132444623/insights/post_reactions_like_total?access_token=EAACEdEose0cBAAT9ZB1aiGIOA4h3iDbo6JNYDCfCCMe758T7qDJge8GZAwx0BxyOut0xAWxnTlG4PbC2uA8G1uI3s4UayLZARSlW4WBviUpX5Ni5fSZAfbcsWW48aI0nCfdfajDi5vYfv0KYkKoTMnsjLVzFIz3olpHJDwMDBl992zn38AYbefSl9JvG1qgZD")
 
-    render json: @service
+    puts @service
+
+    # save into seed?
+    # save into database?
 
   end
 
