31f1701cd5b37eb3c11351d997db47eaa86c1c54
shirlaine <shirlaine.phang@gmail.com>
2017-12-02 18:13:10 +0800

    Add validations for models

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,6 @@
 class Category < ApplicationRecord
+
   has_many :campaigns
+  validates :name, presence: true
+
 end
--- app/models/facebook_post.rb
+++ app/models/facebook_post.rb
@@ -3,4 +3,6 @@ class FacebookPost < ApplicationRecord
   belongs_to :campaign
   has_many :images, as: :imageable, dependent: :destroy
 
+  validates :title, presence: true
+
 end
